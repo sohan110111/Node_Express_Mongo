72e419754db9670df7e3fe3cb67f05d7db500447
sohan110111 <sohan110111@gmail.com>
2022-12-25 11:02:21 +0600

    file rename successfully created

--- nodeJS/fileSystem.js
+++ nodeJS/fileSystem.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 //console.log(fs);
-fs.appendFile('example.txt', "This is an টেস্ট. New Text added", (err) => {
+fs.rename('example.txt', "rename.text", (err) => {
     if (err)
         console.log(err);
     else {
-        console.log("File successfully created");
-        fs.readFile('example.txt', 'utf8', (err, file) => {
+        console.log("File successfully renamed");
+        //console.log("File successfully created");
+        /*fs.readFile('example.txt', 'utf8', (err, file) => {
             if (err)
                 console.log(err);
             else
                 console.log(file);
-        });
+        });*/
     }
 });
