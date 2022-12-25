fa0643a9cb42c72eff3f5712f766836b6f489e34
sohan110111 <sohan110111@gmail.com>
2022-12-25 12:02:11 +0600

    synchronous file system

--- nodeJS/fileSystem.js
+++ nodeJS/fileSystem.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
+var text = fs.existsSync('rename.text')
+console.log(text);
 //console.log(fs);
-fs.exists('rename.text', (result) => {
-    if (result)
-        //console.log("found");
-        fs.unlink('rename.text', (err) => {
-            if (err)
-                console.log(err);
-            else
-                console.log("File successfully deleted");
-        })
-    else {
-        console.log("not found");
-        //console.log("File successfully created");
-        /*fs.readFile('example.txt', 'utf8', (err, file) => {
-            if (err)
-                console.log(err);
-            else
-                console.log(file);
-        });*/
-    }
-});
+// fs.exists('rename.text', (result) => {
+//     if (result)
+//         //console.log("found");
+//         fs.unlink('rename.text', (err) => {
+//             if (err)
+//                 console.log(err);
+//             else
+//                 console.log("File successfully deleted");
+//         })
+//     else {
+//         console.log("not found");
+//         //console.log("File successfully created");
+//         /*fs.readFile('example.txt', 'utf8', (err, file) => {
+//             if (err)
+//                 console.log(err);
+//             else
+//                 console.log(file);
+//         });*/
+//     }
+// });
